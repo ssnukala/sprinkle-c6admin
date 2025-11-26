f5553382cb63b9a5eb684f839d2896d8d0d8870d
ssnukala <snthelion@gmail.com>
2025-11-26 15:00:50 -0600

    Added Crud6 Dependency

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -1,5 +1,6 @@
 import type { App } from 'vue'
 import { SidebarMenuItems } from './components'
+import CRUD6 from '@ssnukala/sprinkle-crud6'
 
 /**
  * Admin Sprinkle initialization recipe.
@@ -10,6 +11,7 @@ export default {
     install: (app: App) => {
         // Register the SidebarMenuItems component globally
         // This allows layouts to use <SidebarMenuItems /> to display the admin menu
+        app.use(CRUD6)  // Install CRUD6 dependency
         app.component('C6AdminSidebarMenuItems', SidebarMenuItems)
     }
 }
